984b8f8534534e9bd2a3d59ecf337abb38dd6fa6
andi <echo.matuladaandi@gmail.com>
2023-08-01 17:11:02 +0800

    belajar packages dart

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:dart_packages/hello.dart';
+
+void main(List<String> args) {
+  var customer = Customer();
+  var product = Product();
+  var category = Customer();
+
+  print(customer);
+  print(sayHello("andi"));
+}
---
+++ lib/hello.dart
@@ -0,0 +1,6 @@
+library dart_packages;
+
+export 'src/say_hello.dart'; // semua yg ada di sini
+
+export 'src/customer.dart'
+    show Category, Product, Customer; // hanya  Category,Product
